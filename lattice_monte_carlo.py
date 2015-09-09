acf529d5a8ee8cc2a5a783ed5cfab4cfe07ab94e
joseph carmack <joseph.liping@gmail.com>
2015-09-09 11:12:15 -0500

    added the file lattice_monte_carlo.py
    -this file is for the first homework assignment

---
+++ lattice_monte_carlo.py
@@ -0,0 +1,38 @@
+# ising model for homework
+from numpy import *
+
+# global variables
+nx = 50					# number of lattice nodes in x
+ny = 50					# number of lattice nodes in y
+nxy = nx*ny				# total number of lattice sites
+JJ = 0.4				# spin energy (for the ising model
+HH = 0.0				# external magnetic field (ising model)
+kT = 0.05				# boltzmann's constant times temperature
+
+# for ising model initialize spin lattice to random distributions
+
+def ising(cycles):
+		
+	# run simulation
+	
+	for cyc in range[cycles]:
+		for i in range[nxy]:
+			attempt_switch()
+			
+	#magn = sum(sum(spin))
+	
+	# plot
+	return 0;
+
+def
+
+def attempt_switch():
+	
+	# select random lattice site
+	
+	return 0;
+
+def lattice_site_energy():
+	
+	
+	return 0;

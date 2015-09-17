2b993ffedf6fda0749c3c832591ae3be662e3c0a
joseph <joseph.liping@gmail.com>
2015-09-17 13:06:13 -0500

    cleaned up the code, moved to new directory
    - didn't really alter the code much, just made aesthetic
      changes related to plotting and saving figures.
    - moved all the files to a new 'hw1' directory

--- jmc/hw1/ising_magnetic.py
+++ jmc/hw1/ising_magnetic.py
@@ -19,29 +19,36 @@ def ising(cycles):
 			magnetization of the two-state spin system at the end of each
 			monte-carlo cycle
 	"""
+	# import numpy as n
 
 	# declare global variables
-	global nxy,spin,nx,ny,HH,JJ,nA
+	global nxy
+
+	# declare array to store the total magnetization after each cycle
+	magn = zeros(cycles)
 
 	# run simulation for the given number of monte carlo cycles (one cycle =
 	# nx*ny randome monte carlo switch attempts)
 	for cyc in range(cycles):
 		cycle_name = cyc + 1
 		print 'cycle %i completed' % cycle_name
-		
 		for i in range(nxy):
 			attempt_switch()
 
-	return 0;
+	magn = sum(sum(spin))/nxy
+
+	return magn;
 
 def attempt_switch():
 	'''
 	This function attempts to make a switch for a randomely selected spin using
 	the metropolis algorithm.
 	'''
+	# import numpy as n
+	# import random as r
 
 	# declare global variables
-	global nx,ny,spin,nA,nAo
+	global nx,ny,spin
 
 	# select random lattice site (get the indices of the cite in the spin matrix)
 	i = randint(0,nx-1)
@@ -52,8 +59,6 @@ def attempt_switch():
 
 	# make a trial switch to the randomely selected spin
 	spin[i,j] = -spin[i,j] # flip spin
-	nA = nA + spin[i,j]
-	
 	# calculate energy of switched spin
 	e2 = lattice_site_energy(i,j)
 
@@ -65,7 +70,6 @@ def attempt_switch():
 		r2 = random()
 		if r2 > exp(-de/kT): # reject switch
 			spin[i,j] = -spin[i,j]
-			nA = nA + spin[i,j]
 
 	return 0;
 
@@ -75,7 +79,7 @@ def lattice_site_energy(i,j):
 	"""
 
 	# declare global variables
-	global spin,JJ,HH,nx,ny,nA,nAo
+	global spin,JJ,HH,nx,ny
 
 	# get neighbor spin values
 	ie = i + 1		# east
@@ -107,7 +111,7 @@ def lattice_site_energy(i,j):
 	Es = -0.5*JJ*So*Ss
 
 	# External field energy
-	Eo = HH*(nA - nAo)**2.0
+	Eo = -HH*So
 
 	# sum up neighbor interaction and field energies to get total site energy
 	eij = Ee + Ew + En + Es + Eo
@@ -122,11 +126,8 @@ nx = 50					# number of lattice nodes in x
 ny = 50					# number of lattice nodes in y
 nxy = nx*ny				# total number of lattice sites
 JJ = 0.4				# spin energy (for the ising model
-HH = 0.05				# conserve xA parameter
+HH = 0.5				# external magnetic field (ising model)
 kT = 0.1				# boltzmann's constant times temperature
-xA = 0.5				# fraction of A atoms
-nA = int(round(xA*nxy))		# number of A atoms in system
-nAo = nA				# number of A atoms at the outset
 seed()					# seed the random number generator
 
 # for ising model initialize spin lattice to random distributions
@@ -134,7 +135,7 @@ spin = zeros((nx,ny))
 for j in range(ny):
 	for i in range(nx):
 		ran = random()
-		if ran < xA:
+		if ran < 0.4:
 			spin[i,j] = 1
 		else:
 			spin[i,j] = -1
@@ -143,13 +144,16 @@ for j in range(ny):
 # main program
 ###############################################################################
 
-cycles = 50
+cycles = 500
 
-ising(cycles)
+magn = ising(cycles)
 
 # plot final state after monte carlo cycles
 pp.imshow(spin,cmap = cm.winter)
 pp.colorbar()
+title = 'HH = '+str(HH)+', kT = '+str(kT)+', JJ = '+str(JJ)
+pp.title(title)
+pp.clim(-1.0,1.0)
 pp.show()
 ###############################################################################
 # end program

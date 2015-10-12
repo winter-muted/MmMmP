a7ffde0023f668e83a3d999a0542614455930cda
josephcarmack <joseph.liping@gmail.com>
2015-10-12 15:09:28 -0500

    polished up ch2d.py and finished partA of the hw
    - added the directory 'partA' with the code for
      part A of hw2.
    - the only difference between ch2d.py and
      ch2d_con.py is that the first uses phi as the
      order parameter and the second uses c as the
      order parameter

--- jmc/hw2/partA/ch2d_con.py
+++ jmc/hw2/partA/ch2d_con.py
@@ -19,14 +19,13 @@ nxy = nx*ny             # total number of points
 nstr = 0.15             # noise strength
 nsteps = 15000          # number of steps to run sim
 skip = 500		# number of steps to skip between plots
-co = 0.5		# average concenctration of species 'a'
-phio = 2.0*co - 1.0	# average value of phi
+co = 0.25		# average concenctration of species 'a'
 op = nsteps/skip + 1    # number of animation outpus
 
 
 # arrays
 sim_time = arange(0.0,nsteps+skip,skip)*dt
-phi = ones((nx,ny))	# concentration array
+c = ones((nx,ny))	# concentration array
 l = zeros(nsteps/skip+1)# average domain size array
 
 kx1 = mod( 1.0/2 + arange(float(nx))/nx,1.0) -1.0/2
@@ -45,16 +44,16 @@ k4 = k2*k2
 #################################################
 
 # homogenous mixture with noise
-phi = phi*phio		# initializing c right at co
+c = c*co		# initializing c right at co
 noise = random.uniform(-nstr,nstr,(nx,ny))
-phi = phi + noise
+c = c + noise
 
 #################################################
 #		file io stuff   		#
 #################################################
 
 # name of directory to store output vtk files in
-dirname = 'animationfiles'
+dirname = 'ch2d_con_animationfiles'
 
 # delete old vtk files from previous runs
 if os.path.exists('./' + dirname):
@@ -70,9 +69,9 @@ except OSError:
 
 # plot setup
 f1 = plt.figure(1)
-plt.imshow(phi)
+plt.imshow(c)
 plt.colorbar()
-plt.clim(-1,1)
+plt.clim(0,1)
 
 # step function
 
@@ -85,33 +84,33 @@ cnt = 0;                    # animation counter
 for step in range(nsteps):
 
     # calculate df in real space
-    df = phi*phi*phi - phi
+    df = 2.0*c - 6.0*c**2 + 4.0*c**3
 
-    # get the fft of df and phi - phio
+    # get the fft of df and c - co
     df_fft = fft.fft2(df)/nxy
-    phi_fft = fft.fft2(phi)/nxy
+    c_fft = fft.fft2(c)/nxy
 
     # update the CH solution
-    phi_fft = (phi_fft - dt*M*k2*df_fft)/(1.0 + dt*K*M*k4)
+    c_fft = (c_fft - dt*M*k2*df_fft)/(1.0 + dt*K*M*k4)
 
-    # transform phi_fft back to real space to get c again
-    phi = real(fft.ifft2(phi_fft))*nxy
+    # transform c_fft back to real space to get c again
+    c = real(fft.ifft2(c_fft))*nxy
 
-    # plot phi and caclulate average domain size
+    # plot c and caclulate average domain size
 
     if (step+1) % skip == 0 or step == 0 :
         #print out cnt value
         print "step = %i, cnt = %i"%(step+1,cnt)
 
         #plot
-        plt.imshow(phi)
+        plt.imshow(c)
 	plt.title('step = '+str(step+1))
         filename=str(step+1).zfill(10)+'.png'
         plt.savefig(dest_dir +'/'+ filename)
 
         # caclulate average domain size
-        phio_fft = fft.fft2(phi - phio)/nxy
-        Sk = abs(phio_fft)*abs(phio_fft)
+        co_fft = fft.fft2(c - co)/nxy
+        Sk = abs(co_fft)*abs(co_fft)
         kx2 = sum(sum(KX*KX*Sk))/sum(sum(Sk))
         ky2 = sum(sum(KY*KY*Sk))/sum(sum(Sk))
         lx = 2.0*pi/kx2**0.5
@@ -128,7 +127,7 @@ def fitfunct(t,A,n):
 fitPar,fitCov = curve_fit(fitfunct,sim_time,l)
 t = linspace(0.0,nsteps*dt)
 lfit = fitPar[0]*t**fitPar[1]
-par_string = '$A =$ '+str(fitPar[0])+', $n =$ '+str(fitPar[1])
+par_string = '$A =$ '+str(fitPar[0])+', $n =$ '+str(fitPar[1])+', $C_A = $'+str(co)
 
 # plotting
 f2 = plt.figure(2)
@@ -137,9 +136,8 @@ plt.plot(sim_time,l,'ko',t,lfit,'b')
 plt.title('average domain size data with curve of the\nform $A*t^n$ fit to the data')
 plt.xlabel('time in reduced units')
 plt.ylabel('$l(t)$')
-print par_string
 ax.annotate(par_string,xy=(400.0,15.0))
-plt.savefig('adsplot.png')
+plt.savefig('adsplot_ch2d_con.png')
 
 # play an animation of the simulation
-os.system("animate -delay 30 animationfiles/*.png & proc=$!")
+os.system("animate -delay 30 ch2d_con_animationfiles/*.png & proc=$!")

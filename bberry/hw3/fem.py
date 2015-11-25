5c54aa04c1e60edf246151c49d4b477ecb7a8de4
Bruce Berry <brcbrr@gmail.com>
2015-11-25 13:25:27 -0600

    finished hw.

--- bberry/hw3/fem.py
+++ bberry/hw3/fem.py
@@ -1,17 +1,13 @@
-# Do some global imports
+# Do some library imports
 from __future__ import division
 import numpy as np
-
 import matplotlib.pyplot as plt
-import os
-from multiprocessing import Process
-import copy
+
 
 elems = 16
 nodes = elems + 1
 h = 5.0/elems
-w = .025
-print "h=",h
+w = -.025
 
 alpha = 2.0/h
 beta = -1.0/h
@@ -19,32 +15,39 @@ gamma = w*h/2
 A = np.full(nodes*nodes,0.0).reshape((nodes,nodes))
 b = np.zeros(nodes)
 
-# Fix row 1
+# Fix first row
 A[0][0] = alpha/2
-# A[0][1] = beta
-# b[0] = gamma
 # general tridiagonal sys
 for i in range(1,nodes-1):
     A[i][i-1] = beta
     A[i][i] = alpha
     A[i][i+1] = beta
-    b[i] = -2*gamma
-
+    b[i] = 2*gamma
 # fix end row
-# A[nodes-1][nodes-2] = beta
 A[nodes-1][nodes-1] = alpha/2
-# b[nodes-1] = gamma
-
-
-
-print "A=\n",A
-print "b=\n",b
-
+# solve
 x = np.matmul(np.linalg.inv(A),b)
 
-print "x=\n",x
-print np.min(x)
+t1 = np.linspace(0,5,nodes)
+t2 = np.linspace(0,5,250)  # change to nodes to get proper error evaluation
+y = (w/2)*(-t2*t2 + 5*t2)
+# residual error and minima
+r = np.zeros(nodes)
+# r = y - x
+# print "error = ",np.sum(r)
+# print np.min(x)
+# print np.min(y)
 
 
-plt.plot(x)
-plt.show()
+# plot
+
+plt.figure(figsize=(8,4.5))
+plt.plot(t1,x,marker='o',linestyle='--',color='red',label='FEM')
+plt.plot(t2,y,color='blue',label='Analytic')
+plt.title('4 elements')
+plt.xlabel('Position (ft)')
+plt.ylabel('Displacement (ft)')
+plt.legend()
+
+# plt.show()
+plt.savefig('16-elems.png')

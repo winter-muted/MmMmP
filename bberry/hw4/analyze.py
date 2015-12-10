6a9502ae1e13462d04b5b0b3bc43ac5afb1baebc
Bruce Berry <beberry@email.uark.edu>
2015-12-10 12:42:52 -0600

    updated script again.

--- bberry/hw4/analyze.py
+++ bberry/hw4/analyze.py
@@ -9,44 +9,36 @@ def fit_func(x,A,B):
 
 
 
-
 filename = 'sigma33-small.txt'
-strain_rate = .0001538 # .02 / 130 angstroms
+strain_rate = .0001538*2 # .02 / 130 angstroms, engineering strain
+energy_factor = 160.2 # eV/ang^3 to GPA
 # strain_rate = 0
 sim_steps = 30000
 strain_interval = 20
 plot_interval = 5
 
-elastic_range = 1000 # visually determined end of elastic deformation
+# elastic_range = 1350 # visually determined end of elastic deformation
+elastic_range = 1050
 
 data = np.genfromtxt(filename,delimiter=',',names=['x','y'])
 
-strain_x = np.linspace(0,strain_rate*plot_interval/(strain_interval),sim_steps/plot_interval + 1)
+strain_x = data['x']*(strain_rate*plot_interval/strain_interval)
 
-fit_x = np.linspace(0,strain_rate*plot_interval/(strain_interval),elastic_range)
-fit_data = data['y'][0:elastic_range]
+fit_x = np.linspace(0,strain_x[elastic_range])
+fit_data = data['y'][0:elastic_range]*energy_factor
 
 
-
-params = curve_fit(fit_func,fit_x,fit_data)
+params = curve_fit(fit_func,strain_x[0:elastic_range],fit_data)
 [E,C] = params[0]
 # #
 fit_y = fit_x*E + C
-#
-# print("Slope = %f" % E)
-# print(params[0])
-print(fit_x.size)
-print(fit_data.size)
-print(strain_x.size)
-print(data['y'].size)
-# for i in range(0,20):
-#     print(fit_x[i],strain_x[i])
+print ("Young's Modulus = %f" % E)
 
-
-plt.xlabel('Elongation Factor')
-plt.ylabel('sigma-zz (Pa/angstrom)')
-plt.plot(fit_x,fit_data)
-plt.plot(strain_x,data['y'])
-# # plt.legend()
+plt.title('8x8x36 Z stress')
+plt.xlabel('Strain')
+plt.ylabel('sigma-zz (Pa)')
+line1 = plt.plot(strain_x,data['y']*energy_factor)
+line2 = plt.plot(strain_x[0:elastic_range],fit_data,fit_x,fit_y)
+plt.legend(['Data','E=%f' %E])
 plt.show()
-# # plt.savefig('sigma33-small.png')
+plt.savefig('sigma33-small-fit.png')
